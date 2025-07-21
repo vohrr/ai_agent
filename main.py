279c79195eb7a8f51d73be315b64bc8d46e22f2b
vohrr <matthew.d.pingel@gmail.com>
2025-07-21 16:07:56 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    model = 'gemini-2.0-flash-001'
+    contents = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    response = client.models.generate_content(model=model, contents=contents)
+    print(response.text)
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
